be4ab81d5f617b6b75562f813821e2bf3e337a7f
ysun7 <ysun7@beautifulstore.org>
2022-01-13 21:40:31 +0900

    #20220113 validate business number

---
+++ validate.go
@@ -0,0 +1,30 @@
+package validate_business_number
+
+import "strconv"
+
+func ValidateBusinessNumber(bisNo string) bool {
+	var arrCheckNum = []int{1, 3, 7, 1, 3, 7, 1, 3, 5}
+
+	sum := 0
+	if len(bisNo) == 10 {
+		for i := 0; i < 9; i++ {
+			r, _ := strconv.Atoi(string(bisNo[i]))
+			sum += r * arrCheckNum[i]
+		}
+		t, _ := strconv.Atoi(string(bisNo[8]))
+		sum += (t * arrCheckNum[8]) / 10
+		sum = 10 - (sum % 10)
+
+		lastNum, _ := strconv.Atoi(string(bisNo[9]))
+
+		if sum == lastNum {
+			return true
+		} else {
+			return false
+		}
+	} else {
+		return false
+	}
+
+}
+
---
+++ validate_test.go
@@ -0,0 +1,36 @@
+package validate_business_number
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestValidateBusinessNumber_10자리아닌경우(t *testing.T) {
+	//given
+	a := "123456789"
+	//when
+	actual :=  ValidateBusinessNumber(a)
+	//then
+	assert.Equal(t, false, actual)
+	assert.Equal(t, len(a), 9)
+}
+
+
+func TestValidateBusinessNumber_유효한번호아닌경우(t *testing.T) {
+	//given
+	a := "1234567890"
+	//when
+	actual :=  ValidateBusinessNumber(a)
+	//then
+	assert.Equal(t, false, actual)
+}
+
+func TestValidateBusinessNumber_Ok(t *testing.T) {
+	//given
+	a := "2018121515"
+	//when
+	actual :=  ValidateBusinessNumber(a)
+	//then
+	assert.Equal(t, true, actual)
+}
+
